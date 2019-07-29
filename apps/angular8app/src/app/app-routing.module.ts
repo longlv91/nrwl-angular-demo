614f3bd3da9b4c382b6dede4d1d62a1a9357d245
long lee <long.lee@CZ1CG1.PRI>
2019-07-29 18:10:38 +0700

    Routing module

--- apps/angular8app/src/app/app-routing.module.ts
+++ apps/angular8app/src/app/app-routing.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'apps', pathMatch: 'full'
+  },
+  {
+    path: 'apps', loadChildren: () => import('./modules/application/application.module').then(mod => mod.ApplicationModule)
+  },
+  {
+    path: 'pages', loadChildren: () => import('./modules/page/page.module').then(mod => mod.PageModule)
+  },
+  {
+    path: 'user-interface', loadChildren: () => import('./modules/ant-elements/ant-elements.module').then(mod => mod.AntElementsModule)
+  },
+  { path: '**', redirectTo: 'apps' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- apps/angular8app/src/app/modules/application/application-routing.module.ts
+++ apps/angular8app/src/app/modules/application/application-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardsComponent } from './components/dashboards/dashboards.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboards',
+    children: [
+      { path: '', redirectTo: 'analytics', pathMatch: 'full' },
+      {
+        path: 'analytics', component: DashboardsComponent
+      },
+      {
+        path: 'project', component: DashboardsComponent
+      }
+    ]
+    
+  },
+  { path: '*',
+    redirectTo: '/dashboards/analytics',
+    pathMatch: 'full'
+  },
+  {
+    path: 'calendar',
+    component: CalendarComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
